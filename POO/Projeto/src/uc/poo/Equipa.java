d6b565c31d0e65698befdbad18dccdc6d127a390
Tomás Mendes <43079732+tomasbm07@users.noreply.github.com>
2020-12-16 19:14:04 +0000

    no more encoding bugs!

--- POO/Projeto/src/uc/poo/Equipa.java
+++ POO/Projeto/src/uc/poo/Equipa.java
@@ -275,7 +275,7 @@ class Equipa {
     }
 
     private ArrayList<Publicacao> organizarPubs(ArrayList<Publicacao> pubs) {
-        //pubs.sort(new comparador()); // == Colections.sort(pubs, new comparador());
+        pubs.sort(Comparator.comparing(Publicacao::getAno).thenComparing(Publicacao::getTipo).thenComparing(Publicacao::fatorImpacto));
         return pubs;
     }
 
